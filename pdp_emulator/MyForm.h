5d40d5dc7a82c1d89e16793770bc82f768eafb16
artemrudoj <artem.rudoj@frtk.ru>
2016-01-10 00:19:44 +0300

    add text views

--- pdp_emulator/MyForm.h
+++ pdp_emulator/MyForm.h
@@ -2,6 +2,8 @@
 #include "DisassemblerFuncImpl.h"
 #include "EmulatorFuncImpl.h"
 #include "SimpleDisplay.h"
+#include "OppCodeGenerator.h"
+#include <iomanip>
 namespace pdp_emulator {
 
 	using namespace System;
@@ -17,17 +19,58 @@ namespace pdp_emulator {
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	private:	Emulator* emulator;
-				SimpleDisplay^ display;
+	private:	OppCodeGenerator* oppCodeGenerator;
+	private:	int  currentPosition;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::Label^  label6;
+
+			 SimpleDisplay^ display;
 	public:
+		template <class T>
+string to_string(T t, ios_base & (*f)(ios_base&))
+{
+  ostringstream oss;
+  oss << f << t;
+  return oss.str();
+}
 		MyForm(void)
 		{
 			InitializeComponent();
 			emulator = new Emulator();
+			oppCodeGenerator = new OppCodeGenerator();
 			display = gcnew SimpleDisplay();
+			currentPosition = 0;
 		}
+
+		String^ toHex(uint16_t num) {
+			char tmp[20];
+			sprintf(tmp, "%X\n", num);
+			String^ tmpString = gcnew String(tmp);
+			return tmpString;
+		}
+		String^ toBin(uint16_t num) {
+			char tmp[20];
+			itoa(num, tmp, 2);
+			String^ tmpString = gcnew String(tmp);
+			return tmpString;
+		}
+
+
 		void startSimpleDisplay() {
 			display->setUpSimpleDisplay(this->pictureBox1, (uint8_t *)emulator->getVideoMemory());
 			display->startDisplaying();
+			uint16_t* raw = oppCodeGenerator->testGenerate();
+			label1->Text = toHex(currentPosition);
+			label2->Text = toBin(raw[currentPosition]);
+			label3->Text = toHex(currentPosition+1);
+			label4->Text = toBin(raw[currentPosition + 1]);
+			label5->Text = toHex(currentPosition+2);
+			label6->Text = toBin(raw[currentPosition + 2]);
+			currentPosition++;
 		}
 
 	protected:
@@ -60,6 +103,12 @@ namespace pdp_emulator {
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
@@ -69,7 +118,7 @@ namespace pdp_emulator {
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"next";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
@@ -81,22 +130,91 @@ namespace pdp_emulator {
 			this->pictureBox1->TabIndex = 1;
 			this->pictureBox1->TabStop = false;
 			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(348, 82);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"label1";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(421, 82);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(35, 13);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"label2";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(348, 99);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(35, 13);
+			this->label3->TabIndex = 4;
+			this->label3->Text = L"label3";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(421, 99);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(35, 13);
+			this->label4->TabIndex = 5;
+			this->label4->Text = L"label4";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(348, 116);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(35, 13);
+			this->label5->TabIndex = 6;
+			this->label5->Text = L"label5";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(421, 116);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(35, 13);
+			this->label6->TabIndex = 7;
+			this->label6->Text = L"label6";
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(364, 326);
+			this->ClientSize = System::Drawing::Size(625, 326);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
 		startSimpleDisplay();
 	}
-	};
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+};
 }
